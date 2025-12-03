935aa4f8ca4e05c0c259104e584b1b314f73a1f1
Mateusz <mateusz.zdrod@gmail.com>
2025-12-03 02:59:15 +0100

    smallprintfbug

--- mini_talk.h
+++ mini_talk.h
@@ -11,27 +11,29 @@
 /* ************************************************************************** */
 
 #ifndef MINI_TALK_H
-# define MINI_TALK_H
+#define MINI_TALK_H
 
-# include "../libft/libft.h"
-# include <signal.h>
+#include "../libft/libft.h"
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 typedef struct s_buffer
 {
-	char					buffer;
-	int						size;
-}							t_buffer;
+	char buffer;
+	int size;
+} t_buffer;
 
 typedef struct s_data
 {
-	char					*message;
-	char					*p;
-	char					buffer;
-	int						buffer_size;
-	int						pid;
-	int						bytes_size;
-	int						bytes_send;
-	volatile sig_atomic_t	finished;
-}							t_data;
+	char *message;
+	char *p;
+	char buffer;
+	int buffer_size;
+	int pid;
+	int bytes_size;
+	int bytes_send;
+	volatile sig_atomic_t finished;
+} t_data;
 
 #endif
